69f61f5402ae51e4f3e4a1e0cab5c6bd3b18b23e
sgrsn <sgrsn1711@gmail.com>
2020-07-09 19:48:17 +0900

    first commit

--- article_translate.py
+++ article_translate.py
@@ -1,48 +1,28 @@
+import sys
+from kibela_selenium.kibela import KibelaDriver
+from deepL_selenium.deepL import DeepLDriver
+
 if __name__ == '__main__':
+
     kibela_url = 'https://hidaka.kibe.la'#/groups/2'
-    my_kibela = KibelaDriver(kibela_url, 'sgrsn1711@gmail.com', 'HidakaKB1711')
-    #article = my_kibela.copy_article('https://hidaka.kibe.la/notes/101')
+    password = sys.argv[1]
+
+    # kibrlaにログイン
+    my_kibela = KibelaDriver(kibela_url, 'sgrsn1711@gmail.com', password)
+
+    # kibelaから記事をコピー
     article_url = 'https://hidaka.kibe.la/notes/142'
-    article = my_kibela.copy_article(article_url)
-    my_corpus = article['text'].split('\n')    #['en', 'en'...]のリスト
+    tmp_corpus = my_kibela.get_corpus(article_url)
 
-    tmp_corpus = my_corpus.copy()
-
-    # 翻訳の邪魔なので画像を除外
-    img_list = []
-    for sentence in my_corpus:
-        if re.match('<img title=.*>', sentence):
-            img_list.append(sentence)
-            tmp_corpus.remove(sentence)
-
-    tmp_corpus.append(article['title'])
-    print("size: ", len(tmp_corpus))
-    
-    # Create translation object
+    # deepLで翻訳
     my_driver_path = 'C:\\Users\\i7-860\\OneDrive\\デスクトップ\\chromedriver_win32\\chromedriver'
-    deepL = seleniumDeepL(driver_path=my_driver_path, loglevel='info')
-
-    deepL.run_translation(corpus=tmp_corpus, time_to_translate=25, destination_language='ja',
+    my_deepL = DeepLDriver(driver_path=my_driver_path)
+    my_deepL.run_translation(corpus=tmp_corpus, time_to_translate=25, destination_language='ja',
                           load_and_store_at=None, quit_web=False, raise_error=True)
-    deepL.close_driver()
+    my_deepL.close_driver()
 
-    translation = deepL.get_translated_corpus() # {'en': 'ja', ...}の辞書型
-    ja_text_list = []
-    ja_title = translation[ article['title'] ]
-
-    # 画像を辞書に追加
-    for img_sentence in img_list:
-        translation[img_sentence] = img_sentence
-
-    for i, en_text in enumerate(my_corpus):
-
-        if en_text in translation:
-            ja_text_list.append( translation[en_text] ) 
-
-        else:
-            print('keyがないですね')
-            print('key: ', en_text)
-            
-    ja_texts = '\n'.join(ja_text_list)
-    my_kibela.write_article(ja_title, ja_texts, pub=False)
+    # kibelaに翻訳結果を書き込み
+    translation = my_deepL.get_translated_corpus() # {'en': 'ja', ...}の辞書型
+    ja_title, ja_text = my_kibela.get_translated(translation)
+    my_kibela.write_article(ja_title, ja_text, pub=False)
     #my_kibela.close()
